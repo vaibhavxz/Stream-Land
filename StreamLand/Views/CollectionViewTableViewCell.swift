21b178403931d94a8d17e03bc669a7abfe701729
Vaibhav <vaibhavchoudhary@Vaibhavs-MacBook-Air-2.local>
2024-08-05 00:24:37 +0530

    added collectnView to tableViewq

---
+++ StreamLand/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  StreamLand
+//
+//  Created by Vaibhav on 04/08/24.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+    
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemBlue
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+}
